e4240aeae3aa279ae829100f28b30b27627538f3
mazengh <mazengh@gmail.com>
2020-03-27 19:30:15 -0700

    added frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const ajax = Axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: `${location.host}/api`, //"http://localhost:8080/api",
   timeout: 60 * 1000
 });
 
